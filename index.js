0ce06c9bd888b62fca5be7be917d11d375f0aa03
Huyh Huyhu <2509roblox@gmail.com>
2023-02-23 00:26:54 +0700

    aef

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import router from './router/router.js';
 const app = express();
 app.use(cors());
+
+app.get("/", (req, res) => res.send("hello"));
 app.use(express.json())
 app.use(router)
 app.listen(5000, () => {
